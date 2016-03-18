53667a9f8401e37eb7a26454eedf5ee762ca3e09
andrewheusser <andrew.heusser@gmail.com>
2016-03-18 13:26:21 -0400

    navbar active

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -1,4 +1,12 @@
 angular.module('AOIApp')
-  .controller('navbarController', ['$scope', 'userService', function($scope, userService){
+  .controller('navbarController', ['$scope', '$location', 'userService', function($scope, $location, userService){
       $scope.user = userService.user;
+
+      $scope.isActive = function(path){
+      var currentPath = $location.path().split('/')[1];
+      if (currentPath.indexOf('?') !== -1) {
+        currentPath = currentPath.split('?')[0];
+      }
+      return currentPath === path.split('/')[1];
+      };
 }]);

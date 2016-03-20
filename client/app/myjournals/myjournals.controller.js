534d018e03f5f4a5eeff0dcd06a577a99cc45773
andrewheusser <andrew.heusser@gmail.com>
2016-03-20 08:57:05 -0400

    my journals works!

--- client/app/myjournals/myjournals.controller.js
+++ client/app/myjournals/myjournals.controller.js
@@ -1,32 +1,7 @@
 angular.module('AOIApp')
-  .controller('myjournalsCtrl', ['$scope', 'pubMedService', 'userService', function($scope, pubMedService, userService) {
-
-  MY_SCOPE = $scope;
+.controller('myjournalsCtrl', ['$scope', 'pubMedService', 'userService', 'databaseService', function($scope, pubMedService, userService, databaseService) {
 
   $scope.user = userService.user;
-  $scope.journals = userService.user.myjournals;
-
-  formatJournals = (journals) => {
-    formattedJournals = '';
-    $scope.journals.forEach((journal) => {
-      formattedJournals = formattedJournals + journal.shortTitle + "[JOURNAL] OR "
-    })
-    return formattedJournals.slice(0,-4)
-  }
-
-  // $scope.formattedJournals = formatJournals(userService.user.myjournals)
-  //
-  // getArticles = (search) => {
-  //   databaseService.get(search)
-  //   .success((data)=>{
-  //     $scope.myarticles = data;
-  //     $scope.loading = false;
-  //   }).catch((err)=>{
-  //     console.log(err)
-  //   })
-  // }
-  //
-  // getArticles($scope.formattedJournals)
 
   // $scope.myarticles =
 

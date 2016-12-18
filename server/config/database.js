0a528e5fe8153b9de98c0c55b4323056f250ec79
andrewheusser <andrew.heusser@gmail.com>
2016-12-18 10:46:29 -0500

    removed mongopath

--- server/config/database.js
+++ server/config/database.js
@@ -1,6 +1,6 @@
 // config/database.js
 module.exports = {
 
-    'url' : 'mongodb://davachilab:davachilab@ds061375.mongolab.com:61375/articles_of_interest' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'url' : PROCESS.ENV.DB_ADDR // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
